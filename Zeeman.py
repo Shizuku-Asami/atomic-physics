289898d2220d52d73278424be250be15fd2a0a6c
Shizuku-Asami <31030499+Shizuku-Asami@users.noreply.github.com>
2020-11-05 12:54:02 +0200

    Update Zeeman.py
    Code optimization, type annotation for methods, renamed class attributes, updated the docstring of `g_factor`.

--- Zeeman.py
+++ Zeeman.py
@@ -9,61 +9,65 @@ class Zeeman:
     ----------
     spin_multiplicity : int
         The spin multiplicity of the given spectral line.
-    L : int
+    orbital_angular_momentum : int
         The orbital angular momentum of the given spectral line.
-    J : float
+    total_angular_momentum : float
         The total angular momentum of the given spectral line.
     """
-    def __init__(self, spin_multiplicity, L, J):
-        self.spin_multiplicity : int = spin_multiplicity
-        self.L : int = L
-        self.J : float = J
+    def __init__(self, spin_multiplicity, orbital_angular_momentum, total_angular_momentum):
+        self.spin_multiplicity: int = spin_multiplicity
+        self.orbital_angular_momentum: int = orbital_angular_momentum
+        self.total_angular_momentum: float = total_angular_momentum
 
     @property
-    def S(self):
+    def spin(self) -> float:
         """Determines the spin of a given spectral line."""
         return (self.spin_multiplicity - 1) / 2
 
     @property
-    def MJ(self):
+    def mj(self) -> list:
         """Determines the possible projections of the total angular momentum."""
-        mj = []
-        for i in range(int(2*self.J + 1)):
-            mj.append(self.J - i)
+        mj = [self.total_angular_momentum - i for i in range(int(2 * self.total_angular_momentum + 1))]
         return mj
 
     @property
-    def ML(self):
+    def ml(self) -> list:
         """Determines the possible projections of the orbital angular momentum."""
-        ml = []
-        for i in range(int(2*self.L + 1)):
-            ml.append(self.L - i)
+        ml = [self.orbital_angular_momentum - i for i in range(int(2 * self.orbital_angular_momentum + 1))]
         return ml
 
     @property
-    def g_factor(self):
-        """Calculates the Lande Factor"""
-        return 1 + (( (self.J * (self.J + 1)) + (self.S * (self.S + 1)) - (self.L * (self.L + 1)) )/( 2 * self.J * (self.J + 1) ))
+    def g_factor(self) -> float:
+        r"""Calculates the Lande Factor
+        
+        The Lande factor is given by the formula `$$ g_{J} = 1 + \frac{J(J+1) + S(S+1) - L(L+1)}{2J(J+1)} $$`
+        """
+        lande_factor = 1 + (
+                (
+                    (self.total_angular_momentum * (self.total_angular_momentum + 1)) + 
+                    (self.spin * (self.spin + 1)) - 
+                    (self.orbital_angular_momentum * (self.orbital_angular_momentum + 1))
+                ) /
+                ( 2 * self.total_angular_momentum * (self.total_angular_momentum + 1) )
+            )
+        return lande_factor
 
-    def delta_MJ(self, other):
-        """Calculates MJ_2 - MJ_1 given another spectral line."""
-        delta = []
-        for MJ_1 in self.MJ:
-            _delta = []
-            for MJ_2 in other.MJ:
-                _delta.append(MJ_2 - MJ_1)
-            delta.append(_delta)
+    def delta_mj(self, other) -> list:
+        """Calculates mj_2 - mj_1 given another spectral line."""
+        delta = [[mj_2 - mj_1 for mj_2 in other.mj] for mj_1 in self.mj]
         return delta
 
 
 if __name__ == "__main__":
     a1 = Zeeman(1, 2, 2)
     a2 = Zeeman(1, 3, 3)
-    print(f'a1 : S = {a1.S}, L = {a1.L}, J = {a1.J}')
-    print(f'a2 : S = {a2.S}, L = {a2.L}, J = {a2.J}')
+    print(f'a1 : S = {a1.spin}, L = {a1.orbital_angular_momentum}, J = {a1.total_angular_momentum}')
+    print(f'a2 : S = {a2.spin}, L = {a2.orbital_angular_momentum}, J = {a2.total_angular_momentum}')
     print("a1.gJ =", a1.g_factor)
     print("a2.gJ =", a2.g_factor)
-    print("a1.ML =", a1.ML)
-    print("a2.ML =", a2.ML)
-    print("a1.MJ =", a1.MJ)
-    print("a2.MJ =", a2.MJ)
+    print("a1.ml =", a1.ml)
+    print("a2.ml =", a2.ml)
+    print("a1.ml =", a1.mj)
+    print("a2.ml =", a2.mj)
+    print(a1.delta_mj(a2))
+    print(a2.delta_mj(a1))
